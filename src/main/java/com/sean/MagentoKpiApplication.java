6ab897c58b0df70a970b8ec1880727b415780a49
sean nguyen <seanynguyenn@gmail.com>
2017-01-15 15:14:21 +0700

    First commit

---
+++ src/main/java/com/sean/MagentoKpiApplication.java
@@ -0,0 +1,12 @@
+package com.sean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagentoKpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagentoKpiApplication.class, args);
+	}
+}
